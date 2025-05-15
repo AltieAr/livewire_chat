03052a6451d678867bd577bb15d014e766e84e5b
AltieAr <suryaprasetya750@gmail.com>
2025-05-15 07:40:53 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/', // pastikan path root
+    build: {
+        manifest: true,
+        outDir: 'public/build', // hasil build disimpan di sini
+        rollupOptions: {
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
