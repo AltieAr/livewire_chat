1bb7aff1faad238a184e7adfc9db8f8ee2107a4a
AltieAr <suryaprasetya750@gmail.com>
2025-05-15 11:25:28 +0700

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/', // pastikan path root
     build: {
         manifest: true,
-        outDir: 'public/build', // hasil build disimpan di sini
+        outDir: 'public/build',
+        emptyOutDir: true,
         rollupOptions: {
             input: [
                 'resources/css/app.css',
